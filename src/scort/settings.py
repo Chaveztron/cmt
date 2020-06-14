052f13237c6af86e7c9116dd2fbfbdeb5bfebe54
Jesus Chavez <jesus.chavez.caro@uabc.edu.mx>
2020-06-14 16:15:40 -0700

    Debug false

--- src/scort/settings.py
+++ src/scort/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^_)2)ii4gxf7s5$76)x_3w0!pp+-$o8_8(+ooqp6@16ci&%-%3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
